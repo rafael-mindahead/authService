e2fc371d4512f3c139d4ad8cbaa84a4e1635093a
rafael <treze01670@gmail.com>
2026-09-13 00:58:57 -0300

    eat: setup FastAPI, PostgreSQL, Redis and Alembic

---
+++ app/main.py
@@ -0,0 +1,38 @@
+from fastapi import Depends ,FastAPI
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+
+from app.database.database import get_db
+
+app = FastAPI(
+    title="auth-service",
+    description="Authentication and Authorization API",
+    version="1.0.0",
+)
+@app.get("/")
+def root():
+    return {
+        "service": "AuthService",
+        "status": "running",
+    }
+
+@app.get("/health")
+def health():
+    return{
+        "status": "healthy"
+    }
+
+@app.get("/database/health")
+def database_health(
+    db: Session = Depends(get_db)
+):
+    result = db.execute(
+        text("SELECT 1")
+    )
+    return {
+        "database": "PostgreSQL",
+        "status": "connected",
+        "result": result.scalar()
+    }
+
+
---
+++ app/models/user.py
@@ -0,0 +1,57 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import Boolean, DateTime, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True
+    )
+
+    name: Mapped[str] = mapped_column(
+        String(120),
+        nullable=False
+    )
+
+    email: Mapped[str] = mapped_column(
+        String(255),
+        unique=True,
+        index=True,
+        nullable=False
+    )
+
+    password_hash: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False
+    )
+
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True,
+        nullable=False
+    )
+
+    is_verified: Mapped[bool] = mapped_column(
+        Boolean,
+        default=False,
+        nullable=False
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+        nullable=False
+    )
